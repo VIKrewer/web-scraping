c1a1df9249da6da26d5ee534fe91d874df18e245
VIKrewer <160676244+VIKrewer@users.noreply.github.com>
2026-01-01 19:38:24 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ if resposta.status_code == 200:
         titulo = book.h3.a['title']
         preco = book.find('p', attrs={'class': 'price_color'}).text
         link = book.h3.a['href']
-        print(f"Titulo: {titulo}\nPreço: {preco}\nLink: https://books.toscrape.com/{link}")
+        print(f"Titulo: {titulo}\nPreço: {preco}\nLink: https://books.toscrape.com/{link}\n")
+
+    print('='*50, '\nLivros acima de £50.00\n', '='*50, '\n')
+
+    for book in book_article:
+        titulo = book.h3.a['title']
+        preco = float(book.find('p', attrs={'class': 'price_color'}).text.strip('Â£'))
+        link = book.h3.a['href']
+        if preco >= 50:
+            print(f"Titulo: {titulo}\nPreço: £{preco}\nLink: https://books.toscrape.com/{link}\n")
+        
 else:
     print(resposta.status_code)
